5212d84f06b1288aa711847f7d64621cdff31dff
Frank <65928334+FrankSteglinski@users.noreply.github.com>
2022-10-28 18:59:18 -0700

    Add files via upload

---
+++ tDiagramPractice.js
@@ -0,0 +1,25 @@
+//Use a t-diagram to keep track of the following snakepit of a variable assignment exercise.
+
+var num1 = 20;
+var num2 = 5;
+if (num1 < num2) {
+    num2 = num2 * num1;
+} else {
+    num1 = num1 / num2;
+    if (num1 < num2) {
+        num1 = num1 * 2;
+    } else if (num1 == num2) {
+        num2 = num1 * num2;
+    } else {
+        num2 = num2 * 2;
+    }
+}
+console.log(num1);
+console.log(num2);
+
+//Predicted Output: 
+    //8
+    //5
+//Actual Output:
+    //8
+    //5
